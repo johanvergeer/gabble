e32434f47faddc35ae705b005b6166d0a23ba2c5
Johan Vergeer <johan@redgyro.com>
2018-11-10 20:28:50 +0100

    Move title-bar content to title-bar component

--- gabble-frontend/gabble-ui/src/app/app.component.ts
+++ gabble-frontend/gabble-ui/src/app/app.component.ts
@@ -10,27 +10,10 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements OnInit {
   title = 'Gabble';
-  isAuthenticated: boolean;
 
-  constructor(public oktaAuth: OktaAuthService, public router: Router) {
-    // Subscribe to authentication state changes
-    this.oktaAuth.$authenticationState.subscribe(
-      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
-    );
+  constructor() {
   }
 
   async ngOnInit() {
-    // Get the authentication state for immediate use
-    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
-  }
-
-  login() {
-    this.oktaAuth.loginRedirect('/profile');
-  }
-
-  async logout() {
-    // Terminates the session with Okta and removes current tokens.
-    await this.oktaAuth.logout();
-    this.oktaAuth.loginRedirect();
   }
 }
--- gabble-frontend/gabble-ui/src/app/title-bar/title-bar.component.ts
+++ gabble-frontend/gabble-ui/src/app/title-bar/title-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OktaAuthService} from "@okta/okta-angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-title-bar',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleBarComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated: boolean;
 
-  ngOnInit() {
+  constructor(public oktaAuth: OktaAuthService, public router: Router) {
+    // Subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    // Get the authentication state for immediate use
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {
+    this.oktaAuth.loginRedirect('/profile');
+  }
+
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.oktaAuth.logout();
+    this.oktaAuth.loginRedirect();
   }
 
 }
